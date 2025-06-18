549e12d13137f3cd0ee503db242def8958382176
Maksymilian Pamuła <mpamula@virtuslab.com>
2025-06-18 06:31:53 +0200

    init

---
+++ _app/immutable/nodes/0.CNKIOKKM.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/DIkfn1MZ.js";import{g as s}from"../chunks/BXXzCsCV.js";import{s as p}from"../chunks/DHBmu9uR.js";const c=!1,i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i,ssr:c},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=s(e);p(t,()=>r.children),a(o,e)}export{d as component,u as universal};
